b58fc6bdd4418b8928b705f9344d0ddca986f874
Anders Larsson <anders.larsson@icm.uu.se>
2017-02-22 20:54:45 +0100

    git clone mtbls233

--- cloud_portal/shared/deploy.sh
+++ cloud_portal/shared/deploy.sh
@@ -76,7 +76,7 @@ ansible-playbook -i $ansible_inventory_file \
                  $PORTAL_APP_REPO_FOLDER'/playbooks/jupyter/main.yml'
                  
 # deploy luigi
-echo ansible-playbook -i $ansible_inventory_file \
+ansible-playbook -i $ansible_inventory_file \
                  -e "domain=$domain" \
                  --key-file $PRIVATE_KEY \
                  $PORTAL_APP_REPO_FOLDER'/playbooks/luigi/main.yml'
@@ -91,6 +91,19 @@ ansible-playbook -i $ansible_inventory_file \
                  -e "galaxy_api_key=$galaxy_api_key" \
                  --key-file $PRIVATE_KEY \
                  $PORTAL_APP_REPO_FOLDER'/playbooks/galaxy.yml'
+                                 
+# wait for luigi http response != Bad Gateway
+luigi_url="http://luigi.$domain"
+ansible-playbook -i $ansible_inventory_file \
+                 -e "name=luigi" \
+                 -e "url=$luigi_url" \
+                 $PORTAL_APP_REPO_FOLDER'/playbooks/wait_for_http_not_down.yml'
+                 
+# when luigi is up do git clone data into the container
+ansible-playbook -i $ansible_inventory_file \
+                 --key-file $PRIVATE_KEY \
+                 $PORTAL_APP_REPO_FOLDER'/playbooks/git_clone_mtbls233.yml'
+
                                  
 # wait for jupyter notebook http response != Bad Gateway
 jupyter_url="http://notebook.$domain"
