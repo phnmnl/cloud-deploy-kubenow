7241a18daa49d2ac0427500fcdd470d6d6f3aec9
Anders Larsson <anders.larsson@icm.uu.se>
2017-06-16 07:52:06 +0200

    removed old cloudflare from destroy script

--- cloud_portal/shared/destroy.sh
+++ cloud_portal/shared/destroy.sh
@@ -3,11 +3,6 @@
 # Destroy everything
 cd "$PORTAL_DEPLOYMENTS_ROOT/$PORTAL_DEPLOYMENT_REFERENCE"
 
-# read cloudflare credentials from the cloned submodule private repo
-if [ -f "$PORTAL_APP_REPO_FOLDER/phenomenal-cloudflare/cloudflare_token_phenomenal.cloud.sh" ]; then
-   source "$PORTAL_APP_REPO_FOLDER/phenomenal-cloudflare/cloudflare_token_phenomenal.cloud.sh"
-fi
-
 ansible_inventory_file="$PORTAL_DEPLOYMENTS_ROOT/$PORTAL_DEPLOYMENT_REFERENCE/inventory"
 
 # TODO read this from deploy.sh file
