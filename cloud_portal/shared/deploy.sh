873a730e1d8a47b9b2d213d6a27157139e94aa94
Anders Larsson <anders.larsson@icm.uu.se>
2017-08-15 18:34:18 +0200

    update-galaxy-version-tag

--- cloud_portal/shared/deploy.sh
+++ cloud_portal/shared/deploy.sh
@@ -180,7 +180,7 @@ galaxy_api_key=$(cat "$PORTAL_DEPLOYMENTS_ROOT/$PORTAL_DEPLOYMENT_REFERENCE/gala
 ansible-playbook -i "$ansible_inventory_file" \
                  --key-file "$PRIVATE_KEY" \
                  -e "galaxy_chart_version=0.3.0" \
-                 -e "galaxy_image_tag=:rc_v17.05-pheno_cv1.1.88" \
+                 -e "galaxy_image_tag=:rc_v17.05-pheno_cv1.1.93" \
                  -e "galaxy_admin_password=$TF_VAR_galaxy_admin_password" \
                  -e "galaxy_admin_email=$TF_VAR_galaxy_admin_email" \
                  -e "galaxy_api_key=$galaxy_api_key" \
