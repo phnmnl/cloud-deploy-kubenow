a2f8b1bdec4ca69a0aad7e77ffdffa939ccf3948
Anders Larsson <anders.larsson@icm.uu.se>
2017-08-18 16:11:26 +0200

    revert source and read files

--- cloud_portal/shared/state.sh
+++ cloud_portal/shared/state.sh
@@ -18,10 +18,10 @@ function report_err() {
 trap 'report_err' ERR
 
 # read portal secrets from private repo
-if [ -z "$LOCAL_DEPLOYMENT" ]; then
-   source "$PORTAL_APP_REPO_FOLDER/phenomenal-cloudflare/cloudflare_token_phenomenal.cloud.sh"
-   export SLACK_ERR_REPORT_TOKEN=${cat \"$PORTAL_APP_REPO_FOLDER/phenomenal-cloudflare/slacktoken\"}
-fi
+#if [ -z "$LOCAL_DEPLOYMENT" ]; then
+#   source "$PORTAL_APP_REPO_FOLDER/phenomenal-cloudflare/cloudflare_token_phenomenal.cloud.sh"
+#   export SLACK_ERR_REPORT_TOKEN=${cat \"$PORTAL_APP_REPO_FOLDER/phenomenal-cloudflare/slacktoken\"}
+#fi
 
 # Add terraform to path (TODO) remove this portal workaround eventually
 export PATH=/usr/lib/terraform_0.9.11:$PATH
