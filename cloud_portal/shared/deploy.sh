8cbf6c565598536e5ba9b52aa0866de57ca2ee03
Anders Larsson <anders.larsson@icm.uu.se>
2017-11-11 18:21:06 +0100

    ansible phenomenal playbook

--- cloud_portal/shared/deploy.sh
+++ cloud_portal/shared/deploy.sh
@@ -148,96 +148,33 @@ ansible-playbook -i "$ansible_inventory_file" \
                  --skip-tags "heketi-glusterfs" \
                  "$PORTAL_APP_REPO_FOLDER/KubeNow/playbooks/install-core.yml"
 
-# wait for all pods in core stack to be ready
+# deploy phenomenal
 ansible-playbook -i "$ansible_inventory_file" \
                  --key-file "$PRIVATE_KEY" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/wait_for_all_pods_ready.yml"
-
-if [ -n "$TF_VAR_nfs_vol_size" ]
-then
-  # deploy local host path (if single node kvm)
-  ansible-playbook -i "$ansible_inventory_file" \
-                   --key-file "$PRIVATE_KEY" \
-                   -e "nfs_server=$TF_VAR_nfs_server" \
-                   -e "nfs_vol_size=$TF_VAR_nfs_vol_size" \
-                   -e "nfs_path=$TF_VAR_nfs_path" \
-                   "$PORTAL_APP_REPO_FOLDER/KubeNow/playbooks/install-shared-vol-nfs.yml"
-else
-  # deploy heketi as default
-  ansible-playbook -i "$ansible_inventory_file" \
-                   --key-file "$PRIVATE_KEY" \
-                   "$PORTAL_APP_REPO_FOLDER/KubeNow/playbooks/install-heketi-gluster.yml"
-fi
-
-# phenomenal-pvc
-ansible-playbook -i "$ansible_inventory_file" \
-                   --key-file "$PRIVATE_KEY" \
-                   -e "pvc_name=galaxy-pvc" \
-                   -e "pvc_storage=$TF_VAR_phenomenal_pvc_size" \
-                   "$PORTAL_APP_REPO_FOLDER/KubeNow/playbooks/create-pvc.yml"
-
-# jupyter
-ansible-playbook -i "$ansible_inventory_file" \
-                 --key-file "$PRIVATE_KEY" \
+                 -e "nfs_server=$TF_VAR_nfs_server" \
+                 -e "nfs_vol_size=$TF_VAR_nfs_vol_size" \
+                 -e "nfs_path=$TF_VAR_nfs_path" \
+                 -e "pvc_name=galaxy-pvc" \
+                 -e "pvc_storage=$TF_VAR_phenomenal_pvc_size" \
                  -e "jupyter_chart_version=0.1.2" \
-                 -e "hostname=$jupyter_hostname" \
+                 -e "jupyter_hostname=$jupyter_hostname" \
                  -e "jupyter_image_tag=:latest" \
                  -e "jupyter_password=$TF_VAR_jupyter_password" \
                  -e "jupyter_pvc=galaxy-pvc" \
                  -e "jupyter_resource_req_cpu=200m" \
                  -e "jupyter_resource_req_memory=1G" \
-                 -e "nologging=$no_sensitive_logging" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/jupyter.yml"
-
-# luigi
-ansible-playbook -i "$ansible_inventory_file" \
-                 --key-file "$PRIVATE_KEY" \
-                 -e "hostname=$luigi_hostname" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/luigi/main.yml"
-
-# kubernetes-dashboard
-ansible-playbook -i "$ansible_inventory_file" \
-                 --key-file "$PRIVATE_KEY" \
-                 -e "basic_auth=$dashboard_auth" \
-                 -e "hostname=$dashboard_hostname" \
-                 -e "nologging=$no_sensitive_logging" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/kubernetes-dashboard/main.yml"
-
-# heapster (adds stats to kubernetes-dashboard)
-ansible-playbook -i "$ansible_inventory_file" \
-                 --key-file "$PRIVATE_KEY" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/heapster.yml"
-
-# galaxy
-ansible-playbook -i "$ansible_inventory_file" \
-                 --key-file "$PRIVATE_KEY" \
+                 -e "jupyter_nologging=$no_sensitive_logging" \
+                 -e "luigi_hostname=$luigi_hostname" \
+                 -e "dashboard_basic_auth=$dashboard_auth" \
+                 -e "dashboard_hostname=$dashboard_hostname" \
+                 -e "dashboard_nologging=$no_sensitive_logging" \
                  -e "galaxy_chart_version=0.3.2" \
-                 -e "hostname=$galaxy_hostname" \
+                 -e "galaxy_hostname=$galaxy_hostname" \
                  -e "galaxy_image_tag=:rc_v17.05-pheno_cv1.1.93" \
                  -e "galaxy_admin_password=$TF_VAR_galaxy_admin_password" \
                  -e "galaxy_admin_email=$TF_VAR_galaxy_admin_email" \
                  -e "galaxy_api_key=$galaxy_api_key" \
                  -e "galaxy_pvc=galaxy-pvc" \
-                 -e "postgres_pvc=false" \
-                 -e "nologging=$no_sensitive_logging" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/galaxy.yml"
-
-# wait until jupyter is up and do git clone data into the container
-ansible-playbook -i "$ansible_inventory_file" \
-                 --key-file "$PRIVATE_KEY" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/git_clone_mtbls233.yml"
-
-# wait for jupyter notebook http response != Bad Gateway
-ansible-playbook -i "$ansible_inventory_file" \
-                 -e "name=$jupyter_hostname" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/wait_for_http_not_down.yml"
-
-# wait for luigi http response != Bad Gateway
-ansible-playbook -i "$ansible_inventory_file" \
-                 -e "name=$luigi_hostname" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/wait_for_http_not_down.yml"
-
-# wait for galaxy http response 200 OK
-ansible-playbook -i "$ansible_inventory_file" \
-                 -e "name=$galaxy_hostname" \
-                 "$PORTAL_APP_REPO_FOLDER/playbooks/wait_for_http_ok.yml"
+                 -e "galaxy_postgres_pvc=false" \
+                 -e "galaxy_nologging=$no_sensitive_logging" \
+                 "$PORTAL_APP_REPO_FOLDER/playbooks/install-phenomenal.yml"
